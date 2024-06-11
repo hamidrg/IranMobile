758ad47024fe5baef5aad3666815d77412eedcd1
Hamidreza Ghorbani <104212208+hamidrg@users.noreply.github.com>
2024-06-11 12:19:41 +0330

    Update database_config.php

--- iranmobile/database_config.php
+++ iranmobile/database_config.php
@@ -3,9 +3,9 @@ $db_host = "localhost";
 $db_username = "root";
 $db_pass = "";
 $db_name = "shop_db";
-$port = 3307; 
+#$port = 3307; 
 
-$link = mysqli_connect($db_host, $db_username, $db_pass, $db_name, $port);
+$link = mysqli_connect($db_host, $db_username, $db_pass, $db_name);
 
 if (mysqli_connect_errno()) {
     exit("خطاي با شرح زير رخ داده است: " . mysqli_connect_error());
